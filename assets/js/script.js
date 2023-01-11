f58e655e026cfaaa32fb85c44a94c9096a266d36
BrettSantor <b.santor27@gmail.com>
2023-01-11 13:59:36 -0800

    build out html skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var tmApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=9guoY8HVvZn5Dz76zhZz9omQCGJGNs7n'
+var tmApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?city=denver&countryCode=US&apikey=9guoY8HVvZn5Dz76zhZz9omQCGJGNs7n'
 var weatherApiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&hourly=temperature_2m'
 
 fetch(tmApiUrl).then(function (response) {
@@ -16,3 +16,10 @@ fetch(weatherApiUrl).then(function (response) {
         }) 
     }
 })
+
+
+
+//event listener on submission of city name
+//once submitted modale brings up local events and dates
+//once selected weather is shown for local event
+//function to submit city name and store it into local storage
