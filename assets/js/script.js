1693a25698512d6b661e0cc7f959376f12d924f2
Jonathan Pohahau <jonathanpohahau@Jonathans-MBP.attlocal.net>
2023-01-11 14:30:32 -0800

    added some of the  html classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 var tmApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?city=denver&countryCode=US&apikey=9guoY8HVvZn5Dz76zhZz9omQCGJGNs7n'
 var weatherApiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&hourly=temperature_2m'
+var searchBtn = document.querySelector("#searchBtn")
+
+
 
 fetch(tmApiUrl).then(function (response) {
     if (response.ok) {
