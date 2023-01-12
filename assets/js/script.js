0ed8bed3823314a23ebf4c288aedf21aebc7a02e
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-01-12 10:00:45 -0800

    more psuedo coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 var tmApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?city='+ {city} +'&countryCode=US&apikey=9guoY8HVvZn5Dz76zhZz9omQCGJGNs7n'
 var weatherApiUrl = 'https://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&hourly=temperature_2m'
 //variable for html elements
+// use d3 to select elements from html
+// var weatherInfo 
+// var ticketInfo
+
+
+
 fetch(tmApiUrl).then(function (response) {
     if (response.ok) {
         response.json().then(function (data) {
@@ -19,20 +25,27 @@ fetch(weatherApiUrl).then(function (response) {
 
 
 
-//event listener on search submit of city name
+//event listener on search submit of cityName
     //invoke a function eventSearch
 
-//eventSearch function accepting city name as an argument
-    //fetch ticketmaster using city name
-        //call a function displayEvent
-    // fetch weather using the city name
-        //call a function displayWeather
+//eventSearch function accepting cityName as an argument
+    // fetch ticketmaster using cityName
+        //call a function displayEvent passes data into it
+    // fetch weather using the cityName
+        //call a function displayWeather passes data into it
     
-//create a function called displayEvent
-//create a function called displayWeather
+//function displayEvent accepts data
+// --- display event information --- event name, ticket availability, price range, date, please note
+    // iterate over data --- for events shown on cityName search
+        // creates element for event info
+        // append elements to weatherInfo
 
 
-//once submitted modale brings up local events and dates
+//function displayWeather accepts data
+// --- displays weather --- location, date, temp, condition, condition icon, wind
+
+
+//once submitted modal brings up local events and dates
 //once selected weather is shown for local event
 //function to submit city name and store it into local storage
 //
