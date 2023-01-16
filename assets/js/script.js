48066be149384588fdc5e0dcbaed5f906feb2632
byxz885@gmail.com <53200767+byzESC@users.noreply.github.com>
2023-01-16 00:46:24 -0800

    renaming, refactor, added attribute to eCard, append weather in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
-// var searchBtn = d3.select('#modal-button');
-// var citySearchForm = d3.select('#city-search-form');
+
 var searchBtn = document.getElementById('search-submit-button');
 var citySearchForm = document.getElementById('city-search-input');
 var inputVal = d3.select('#inputState');
 
-var startDate
-var endDate
-var weather
-
-
+// moved global declaration var to line 139
+// changed names for more clarity
+var eventWeather;
+var eventDate;
 var ticketInfo = d3.select('#ticketInfo')
-var date;
-                    
 
 // user enter city name, date range, and pick genre
     // fetch using user input, and area radius
@@ -23,13 +19,7 @@ var date;
 
 // fetch event data function accepting cityName as an argument
 var eventSearch = function (city, genre, startDate, endDate) {
-    console.log(startDate)
-    console.log(endDate)
-
-    var ticketMasterApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?&q=' + city + '&classificationName='+genre+'&startDateTime='+startDate+'T00:00:01Z&endDateTime='+endDate+'T23:59:59Z&radius=50&apikey=9guoY8HVvZn5Dz76zhZz9omQCGJGNs7n'
-    var weatherApiUrl = 'https://api.weatherapi.com/v1/future.json?key=4e031b93d3c141019f0220405231401&q='+city+'&dt='+startDate
-
-   
+    var ticketMasterApiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?&q=' + city + '&classificationName=' + genre + '&startDateTime=' + startDate+ 'T00:00:01Z&endDateTime=' + endDate + 'T23:59:59Z&radius=50&apikey=9guoY8HVvZn5Dz76zhZz9omQCGJGNs7n';
 
     // fetch ticketmaster using cityName
     fetch(ticketMasterApiUrl).then(function (response) {
@@ -42,19 +32,14 @@ var eventSearch = function (city, genre, startDate, endDate) {
     })
 }
 
-// super cool text
-// font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600
-
 //function displayEvent accepts data
 function displayEvent (data, city) {
-    // .classed('card, my-2, mx-4, bg-teal-600, text-white, border, border-black, border-solid, flex-1' , true)
-
-    // creates element for event info
+    // creates elements for event info
     d3.select('#ticketInfo')
     .selectAll('div')
     .data(data)
     .enter()
-    .append('div').attr('id', 'eCard').attr('tabindex', '1').attr('class', 'flex flex-right')
+    .append('div').attr('class', 'eCard').attr('tabindex', '1')
     .each(function(d) {
         d3.select(this).html(
             `<div class="card-body">
@@ -62,112 +47,114 @@ function displayEvent (data, city) {
                 <p class="event-date" value='${d.dates.start.localDate}'>${d.dates.start.localDate}</p>
                 <a href="${d.url}"><button class="inline-block px-2.5 py-1 bg-white font-medium text-teal-600 rounded hover:bg-teal-700">More Info</button></a> 
                 `)
-    .selectChild().insert('p')
-    .text(function(dta) {
-         if(dta.priceRanges){
-         return "Min: $" + dta.priceRanges[0].min + " Max: $" + dta.priceRanges[0].max
-    } else {
-      return "N/A"
-    }
+        .selectChild().insert('p')
+        .text(function(dta) {
+            if(dta.priceRanges){
+                return "Price ranges $" + dta.priceRanges[0].min + " - $" + dta.priceRanges[0].max;
+            } else {
+                return "Price ranges N/A";
+            }
+        });
     });
-});
-    // append elements to weatherInfo
-    d3.select('#ticketInfo').selectAll('#eCard').on('click', function(){
-        date = this.querySelector('.event-date').getAttribute('value')
-         console.log(date)
-        // pass in date and city to fetch weather data
-        // display weather info with d3
-        // append weather info to ticketInfo
-        callWeather(date, city)
-     });
 
+    // append elements to eCard "weatherInfo"
+    d3.select('#ticketInfo').selectAll('.eCard').on('click', function(){
+        // TODO:: selectAll data-selected and remove all data-selected attribute from them
+
+        eventDate = this.querySelector('.event-date').getAttribute('value')
+        // assign a special attribute to --this--
+        // and later could use it to select this particular element to append weather info to
+        d3.select(this).attr('data-selected', 'focused');
+
+        // fetch weather data
+        callWeather(eventDate, city)
+     });
 }
 
-
+// fetch weather data after user select an event
 function callWeather(date, city){
-    var weatherApiUrl = 'https://api.weatherapi.com/v1/future.json?key=4e031b93d3c141019f0220405231401&q='+city+'&dt='+date;
+    var weatherApiUrl = 'https://api.weatherapi.com/v1/future.json?key=4e031b93d3c141019f0220405231401&q=' + city + '&dt=' + date;
 
     fetch(weatherApiUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 // console.log(data);
-                weather = data.forecast.forecastday
-                console.log(weather)
-                displayWeather(weather)
+                eventWeather = data.forecast.forecastday[0];
+                // console.log(weather)
+                displayWeather(eventWeather);
             })
            
         }
-        }) 
+    })
 }
 
 //function displayWeather accepts data
 // --- displays weather --- location, date, temp, condition, condition icon, wind
-function displayWeather (weather) {
-     console.log('date is', date);
-     console.log('this is weather data', weather)
-     var cardBod = d3.select('#eCard');
-                        cardBod.selectAll('article')
-                        .data(weather)
-                        .enter()
-                        .append('article')
-                        .style('border', 'solid')
-                        .text('Temperature: '+weather[0].day.avgtemp_f)
-                    
+function displayWeather (WeatherData) {
+    // .avghumidity, .avgtemp_f, .maxwind_mph, hour[].condition.icon <-- is a URL, hour[].condition.text, .date
+    console.log('date is', eventDate);
+    console.log('this is weather data ', WeatherData)
+
+    //TODO: select eCard element correctly, display weatherData, and append to it
+
+    // .html(`<p>Temperature: ${currentEventWeather.avgtemp_f}</P>`)
+    // var cardBod = d3.select('#eCard');
+    // cardBod.selectAll('p')
+    //         .data(weather)
+    //         .enter()
+    //         .append('p')
+    //         .style('border', 'solid')
+    //         .text('Temperature: ' + currentEventWeather.avgtemp_f)     
     
-    }
-    // avTemp = data.forecast.forecastday[0].day;
-    // displayWeather(avTemp);
-    // var avTemp = data.forecast.forecastday[0].day.avgtemp_f
-    // console.log("temp: ", avTemp)
-    // var locName = data.location.name
-    // console.log("name: ", locName)
-    // var dateW = data.forecast.forecastday[0].date
-    // console.log("date: ", dateW)
-    // var condition = data.forecast.forecastday[0].hour[3].condition.text
-    // console.log("conditon: ", condition)
-    // var conditionIcon = data.forecast.forecastday[0].hour[3].condition.icon
-    // console.log("icon: ", conditionIcon)
-    // var wind = data.forecast.forecastday[0].day.maxwind_mph
-    // console.log("wind: ", wind)
+    var weatherForecast = d3.select('[data-selected]');
+    console.log('eCard element ', weatherForecast._groups[0]);
+    // weatherForecast.select('p').data(data).enter().append('p')
+    //     .attr('class', 'weatherForecast')
+    //     .text('Temperature: ' + currentEventWeather.avgtemp_f);
+    weatherForecast.append('p').html(`Temperature: ${currentEventWeather.avgtemp_f}`)
+    console.log('eCard element ', weatherForecast._groups[0].siblings());
+
+
+
+    // testing with JS codes
+    // var cardEl = document.getElementsByClassName('eCard');
+    // var test = document.getElementById('ticketInfo');
+    // var weatherForecast = document.createElement('p');
+    // weatherForecast.textContent = '-----------------Temperature: ' + currentEventWeather.avgtemp_f;
+    // test.append(weatherForecast);
+    // cardEl.append(weatherForecast);
+}
 
 //event listener on search submit of cityName
-// d3.select('#search-submit-button').on('click', async function (event) {
 d3.select('#search-submit-button').on('click', function (event) {
     event.preventDefault();
     // remove previous search results
-    d3.selectAll('#eCard').remove()
+    d3.selectAll('.eCard').remove()
 
+    // these var gets declare and assign to variables every time --- base on user input
     var city = citySearchForm.value;
     var genre = inputVal.nodes()[0].value
-    var dateRange;
-    startDate = d3.select('#startDate').nodes()[0].value
-    endDate = d3.select('#endDate').nodes()[0].value
+    var startDate = d3.select('#startDate').nodes()[0].value
+    var endDate = d3.select('#endDate').nodes()[0].value
 
-    console.log(startDate)
-    console.log(endDate)
-    console.log(`SEARCHBTN PRESSED searched city is ${city}, genre is ${genre}, and dateRange ${startDate} - ${endDate}`);
-
-    // eventSearch(citySearchForm.value, inputVal.value, startDate, endDate);
-
-
-    
-    // check if city input is empty
-     if (!city) {
-        console.log("need city name")
+    console.log(`SEARCH BUTTON: searched city is ${city}, genre is ${genre}, and dateRange ${startDate} - ${endDate}`);
+     if (!city || !startDate || !endDate) {
+        // check if user entered city, start date, and end date
+        console.log("need city name, start date, end date");
     } else {
-        //invoke a function eventSearch
         eventSearch(city, genre, startDate, endDate);
     }
     citySearchForm.value = "";
 })
 
-// submit-button codes
+// submit-button alteration codes
     // inside else statement
         // displayWeather(avTemp);
         // get the longitude and latitude of the input city
         // var openCageApiUrl =  'https://api.opencagedata.com/geocode/v1/json?q=' + city + '&current_weather=true&key=5ffc6c893abd4262b33abf21d8deab53';
        
         // waits for response from open cage api
+        // d3.select('#search-submit-button').on('click', async function (event) {
         // try{
         //     let response = await fetch(weatherApiUrl);
         //     let data = await response.json();
